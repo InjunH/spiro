f43f8d1c17da09bdd70c4213ac5fc7462408c646
Injun <ijhwnag@whout.co.kr>
2023-09-27 01:17:25 +0900

    차트 변환 파이썬 코드 로거 추가 및 리팩토링 진행

--- analysis_to_chart.py
+++ analysis_to_chart.py
@@ -3,62 +3,49 @@ from datetime import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
-import sys
+from logger_config import setup_logger
 
-# 한글 폰트를 설정합니다.
-font_path = "/System/Library/Fonts/AppleSDGothicNeo.ttc"
-font_name = fm.FontProperties(fname=font_path, size=10).get_name()
-plt.rc('font', family=font_name)
+# Logger 설정
+logger = setup_logger()
 
 # Constants
 BASE_DIR = 'result'
 TODAY = datetime.now().strftime('%Y%m%d')
 CSV_DIR_PATH = os.path.join(BASE_DIR, TODAY)
 
+# 한글 폰트 설정
+font_path = "/System/Library/Fonts/AppleSDGothicNeo.ttc"
+font_name = fm.FontProperties(fname=font_path, size=10).get_name()
+plt.rc('font', family=font_name)
+
 def plot_chart(csv_path):
     df = pd.read_csv(csv_path)
-
-    # 그래프 크기 설정 (기존 크기의 1.5배로 조절)
     plt.figure(figsize=(plt.gcf().get_size_inches()[0] * 1.5, plt.gcf().get_size_inches()[1]))
 
     x_data = df.iloc[:, 0]
     y_data = df.iloc[:, 1]
     color_data = df.iloc[:, 2]
-
     colors = ['b' if value else 'r' for value in color_data]
 
-    bars = plt.bar(x_data, y_data, color=colors)
-
+    plt.bar(x_data, y_data, color=colors)
     plt.xlabel('가장 많이 사용된 단어')
     plt.ylabel('횟수')
     plt.title('단어 사용 빈도')
-    # 범례 색상 변경
     legend_labels = ['True', 'False']
     legend_handles = [plt.Line2D([0], [0], color='b', lw=4), plt.Line2D([0], [0], color='r', lw=4)]
     plt.legend(legend_handles, legend_labels, title='Needed or Not')
-
-    # x축 레이블 간격 조절
     plt.xticks(rotation=45, ha='right', fontsize=10)
-
-    # 그래프 레이아웃 조절
     plt.tight_layout()
 
-    # 현재 날짜를 가져와서 이미지 저장 경로 설정
-    today = datetime.now().strftime('%Y%m%d')
-    folder_path = f"result/{today}"
     image_file_name = os.path.basename(csv_path).replace('.csv', '.png')
-    image_path = os.path.join(folder_path, image_file_name)
+    image_path = os.path.join(CSV_DIR_PATH, image_file_name)
     
-    plt.tight_layout()
     plt.savefig(image_path, dpi=300)
-    print(f"그래프 이미지가 {image_path}에 저장되었습니다.")
-
-    # plt.show()
-    sys.exit()
+    logger.info(f"그래프 이미지가 {image_path}에 저장되었습니다.")
 
 def main():
     if not os.path.exists(CSV_DIR_PATH):
-        print(f"{CSV_DIR_PATH} 폴더가 존재하지 않습니다.")
+        logger.error(f"{CSV_DIR_PATH} 폴더가 존재하지 않습니다.")
         return
 
     for csv_file in os.listdir(CSV_DIR_PATH):
