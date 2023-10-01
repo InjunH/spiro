8688a1f2d64e8067a8c4ad228745ecd4845ede4f
Injun <ijhwnag@whout.co.kr>
2023-10-01 18:11:43 +0900

    로거 초기화 처리

--- logger_config.py
+++ logger_config.py
@@ -2,18 +2,19 @@ import logging
 from logging.handlers import TimedRotatingFileHandler
 
 def setup_logger():
-    # 로거 생성
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
+    logger = logging.getLogger(__name__)
+    if not logger.handlers:
+        # 로거 생성
+        logger.setLevel(logging.INFO)
 
-    # TimedRotatingFileHandler 설정
-    handler = TimedRotatingFileHandler('app.log', when='midnight', interval=1, backupCount=30)
-    handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
-    logger.addHandler(handler)
+        # TimedRotatingFileHandler 설정
+        handler = TimedRotatingFileHandler('app.log', when='midnight', interval=1, backupCount=30)
+        handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+        logger.addHandler(handler)
 
-    # StreamHandler 설정 (콘솔 출력을 위한 핸들러)
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
-    logger.addHandler(console_handler)
+        # StreamHandler 설정 (콘솔 출력을 위한 핸들러)
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+        logger.addHandler(console_handler)
 
     return logger
